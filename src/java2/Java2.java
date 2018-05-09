3a33c9b7fa06ec7d7239bbca9c3278df07d680ca
satoge1718@gmail.com <>
2018-05-09 20:11:33 +0200

    versio 1 del projecte java2

---
+++ src/java2/Java2.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java2;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
+/**
+ *
+ * @author sandra
+ */
+public class Java2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("versió 0.1 del projecte prjava02"); 	
+        try {
+            InetAddress adreça = InetAddress.getLocalHost();
+            String hostname = adreça.getHostName();
+            System.out.println("hostname="+hostname);
+            System.out.println("Nom de l'usuari: " + System.getProperty("user.name"));
+            System.out.println("Carpeta Personal: " + System.getProperty("user.home"));
+            System.out.println("Sistema operatiu: " + System.getProperty("os.name"));
+            System.out.println("Versió OS: " + System.getProperty("os.version"));
+        }
+        catch (IOException e) {
+		System.out.println("Exception occurred");
+        }  
+    
+    }
+    
+}
